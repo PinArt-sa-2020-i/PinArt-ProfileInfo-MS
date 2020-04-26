4cbf828b514d970b1ee93f878ac2881bb1db41a7
tpereza <tpereza@unal.edu.co>
2020-04-26 18:28:09 -0500

    :ambulance: DB Important Fixes

---
+++ Migrations/20200426232131_CompleteDBv9.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PinArt_ProfileInfo_MS.Migrations
+{
+    public partial class CompleteDBv9 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Apellido",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Nombre",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "PasswordHash",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "PasswordSalt",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Username",
+                table: "Users");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Apellido",
+                table: "Users",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Nombre",
+                table: "Users",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: true);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "PasswordHash",
+                table: "Users",
+                type: "longblob",
+                nullable: true);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "PasswordSalt",
+                table: "Users",
+                type: "longblob",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Username",
+                table: "Users",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: true);
+        }
+    }
+}
--- Models/User.cs
+++ Models/User.cs
@@ -6,11 +6,6 @@ namespace PinArt_ProfileInfo_MS.Models
     public class User
     {
         public int Id { get; set; }
-        public string Nombre { get; set; }
-        public string Apellido { get; set; }
-        public string Username { get; set; }
-        public byte[] PasswordHash { get; set; }
-        public byte[] PasswordSalt { get; set; }
         public string Correo { get; set; }
         public bool Eliminado { get; set; }
         public bool Privado { get; set; }
